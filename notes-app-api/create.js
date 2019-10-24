e6841ec689745540750c017460e0282cb0f7991c
Krutarth Chokshi <chokshi.krutarth1234@gmail.com>
2019-10-23 20:21:21 -0700

    refactor the code.

--- notes-app-api/create.js
+++ notes-app-api/create.js
@@ -1,9 +1,8 @@
 import uuid from "uuid";
-import AWS from "aws-sdk";
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import {success, failure} from "./libs/response-lib";
 
-const dynamoDB = new AWS.DynamoDB.DocumentClient();
-
-export function main(event, context, callback) {
+export async function main(event, context, callback) {
     // Event object contatins information from the invoker.
     // Body can be accessed from the event.body which is
     // JSON encoded string.
@@ -34,30 +33,11 @@ export function main(event, context, callback) {
         }
     };
 
-    dynamoDB.put(params, (error, data) => {
-        // Set headers to enable CORS (Cross-Origin Resource Sharing)
-        const headers = {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Credentials": true
-        };
+    try {
+        await dynamoDbLib.call("put", params);
+        return success(params.Item);
+    } catch (e) {
+        return failure({ status: false });
+    }
 
-        // Return status code 500 or error
-        if (error) {
-            const response = {
-                statusCode: 500,
-                headers: headers,
-                body: JSON.stringify({ status: false })
-            };
-            callback(null, response);
-            return;
-        }
-
-        // Return status code 200 and the newly created item
-        const response = {
-            statusCode: 200,
-            headers: headers,
-            body: JSON.stringify(params.Item)
-        };
-        callback(null, response);
-    });
 }
