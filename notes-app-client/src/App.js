182927b036855a1ecb250201823df8b6cfa4f56e
Krutarth Chokshi <chokshi.krutarth1234@gmail.com>
2020-05-09 12:16:57 -0700

    Refine Nav bar and add 404 page for paths that are not found

--- notes-app-client/src/App.js
+++ notes-app-client/src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
-import { Link, Route } from "react-router-dom";
-import { Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { Nav, Navbar, NavItem } from "react-bootstrap";
 import "./App.css";
 import Routes from "./Routes";
+import { LinkContainer } from "react-router-bootstrap";
 
 class App extends Component {
   render() {
@@ -11,10 +12,19 @@ class App extends Component {
         <Navbar fluid collapseOnSelect>
           <Navbar.Header>
             <Navbar.Brand>
-              <Link to="/">Scratch</Link>
+              <Link to = "/">Scratch</Link>
             </Navbar.Brand>
-            <Navbar.Toggle />
           </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav pullRight>
+              <LinkContainer to = "/signup">
+                <NavItem>Signup</NavItem>
+              </LinkContainer>
+              <LinkContainer to = "/login">
+                <NavItem>Login</NavItem>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
         </Navbar>
         <Routes />
       </div>
